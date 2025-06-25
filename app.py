c2325485cc087c273ac7b8df35538d926079f8ea
BoopyBoop <manon.vlieghe@gmail.com>
2025-06-25 12:10:59 +0200

    feat: add /json route returning JSON response with a greeting message

--- app.py
+++ app.py
@@ -1,13 +1,16 @@
-from flask import Flask
+from flask import Flask, jsonify
 from dotenv import load_dotenv
 import os
 
 # Load environment variables from .env file
 load_dotenv()
 
-
 app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
     return "<b>My first Flask application in action!</b>"
+
+@app.route("/json")
+def return_json():
+    return jsonify({"message": "Hello, JSON!"})
